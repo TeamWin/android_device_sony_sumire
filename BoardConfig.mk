be85435d946427cd158774f011c6682ddd89f357
Erik Castricum <erikcas1972@gmail.com>
2016-02-07 12:03:01 +0100

    sumire: call for kitakami BoardConfigOmni.mk
    depends on: https://gerrit.omnirom.org/#/c/16803/
    
    Change-Id: If7da9422f5825f826b2989318783554b4064d4b6
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/kitakami-common/BoardConfig.mk
+include device/sony/kitakami-common/BoardConfigOmni.mk
 include device/sony/common/CommonConfigOmni.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := E6653
